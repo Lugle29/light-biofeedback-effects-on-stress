fe6825bdb82efd1cc5f9c3682fcf36771fe28834
Lugle29 <141414517+Lugle29@users.noreply.github.com>
2024-05-03 12:58:56 +0200

    Update functions.R

--- scripts/functions.R
+++ scripts/functions.R
@@ -1,63 +1,76 @@
+## ---------------------------
+##
+## Script name: functions.R
+##
+## Purpose of script: storing functions for heartrate data management
+##
+## Author: Luis Glenzer
+##
+## Date Created: 2024-03-30
+##
+## ---------------------------
+
 # Setup
+# Load Libraries ---------------------------------------------------------------
 library(hms)
 library(readr)
 library(dplyr)
 library(stringr)
 library(readxl)
 library(writexl)
-library(ggplot2)
 
-# Function to create list with IDs as index. Within are the vectors representing
-# the HRV.
+# Function to create list HRV vectors for each ID
 list_hrv <- function(data, id_data) {
-  ## Prepare List
+  # Prepare --------------------------------------------------------------------
+  # Set Up List
   hrv_list <- list()
   
-  ## Fill List
+  # Assign Values --------------------------------------------------------------
   for (id in id_data$ID) {
     
-    # Subset Dataframe
+    # Subset Dataframe for each participant
     data_sub <- data[data$ID == id,]
     
-    # Get HRV Vectors for light break
+    # Get HRV Vectors for light condition
     ## Subset Data
     data_l   <- data_sub[data_sub$Licht == 'Licht' & data_sub$Training == 'TrainingOn',]
     
-    ## Get Vector
+    ## Get default Vector
     hrv_list[[id]][['light']][['default']] <- data_l$RMSSD_corr
     
     ## Get Vector with Cut out Values
-    ### Subset Data
+    ### Set cut out values
     cut_beg  <- 60 # How many seconds should be cut from the beginning
     data_lt  <- data_l[data_l$Tra_Sek > cut_beg,]
     
-    ### Get Vector
+    ### Get manipulated Vector
     hrv_list[[id]][['light']][['manip']]   <- data_lt$RMSSD_corr
     
-    # Get HRV Vectors for non-light break
+    # Get HRV Vectors for non-light condition
     ## Subset Data
     data_nl   <- data_sub[data_sub$Licht == 'ohneLicht',]
     
-    ## Get Vector
+    ## Get default vector
     hrv_list[[id]][['nlight']][['default']] <- data_sub$RMSSD_corr[data_sub$Licht == 'ohneLicht']
     
     ## Get Vector with Cut out Values
     ### Subset Data
     data_nlt  <- data_nl[data_nl$Tra_Sek > cut_beg,]
     
-    ### Get Vector
-    hrv_list[[id]][['nlight']][['manip']]   <- data_nlt$RMSSD_corr
+    ### Get manipulated Vector
+    hrv_list[[id]][['nlight']][['manip']] <- data_nlt$RMSSD_corr
   }
+  # Return ---------------------------------------------------------------------
   return(hrv_list)
 }
 
-# Function to get the manipulated averaged vectors
+# Function to average manipulated vectors
 avg_vec <- function(vector_list) {
-  # Prepare Objects
-  ## List
+  # Prepare --------------------------------------------------------------------
+  # Set up list to store results
   return_list = list()
   
-  ## Prepare Light and Non-Light Vectors
+  # Set up light and non-light vectors
   avgs_l <- c()
   avgs_nl <- c()
   
@@ -70,11 +83,12 @@ avg_vec <- function(vector_list) {
     avgs_nl <- c(avgs_nl, mean(vec_nl,na.rm = TRUE))
   }
   
-  # Aggregate in List to Return
+  # Aggregate ------------------------------------------------------------------
   return_list[['ID']]     <- names(vector_list)
   return_list[['light']]  <- avgs_l
   return_list[['nlight']] <- avgs_nl
   
+  # Return ---------------------------------------------------------------------
   return(return_list)
 }
 
